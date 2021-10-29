42fef2734a8b736890d11e44880297904261171c
Ulf Hansson <ulf.hansson@linaro.org>
2021-10-29 14:38:55 +0200

    PM: sleep: Fix runtime PM based cpuidle support
    In the cpuidle-psci case, runtime PM in combination with the generic PM
    domain (genpd), may be used when entering/exiting a shared idlestate. More
    precisely, genpd relies on runtime PM to be enabled for the attached device
    (in this case it belongs to a CPU), to properly manage the reference
    counting of its PM domain.
    
    This works fine most of the time, but during system suspend in
    dpm_suspend_late(), the PM core disables runtime PM for all devices. Beyond
    this point, calls to pm_runtime_get_sync() to runtime resume a device may
    fail and therefore it could also mess up the reference counting in genpd.
    
    To fix this problem, let's call wake_up_all_idle_cpus() in
    dpm_suspend_late(), prior to disabling runtime PM. In this way a device
    that belongs to a CPU, becomes runtime resumed through cpuidle-psci and
    stays like that, because the runtime PM usage count has been bumped in
    device_prepare().
    
    Diagnosed-by: Maulik Shah <mkshah@codeaurora.org>
    Suggested-by: Rafael J. Wysocki <rafael@kernel.org>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1498,6 +1498,7 @@ int dpm_suspend_late(pm_message_t state)
 	int error = 0;
 
 	trace_suspend_resume(TPS("dpm_suspend_late"), state.event, true);
+	wake_up_all_idle_cpus();
 	mutex_lock(&dpm_list_mtx);
 	pm_transition = state;
 	async_error = 0;
